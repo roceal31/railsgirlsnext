ba4accecdfd251877a2d5051d5c12d3cd2c5740c
Andrea James <roceal31@gmail.com>
2017-05-27 11:55:53 +1000

    Cleanup and adding Favourites Search

--- music/app/controllers/favourites_controller.rb
+++ music/app/controllers/favourites_controller.rb
@@ -21,6 +21,23 @@ class FavouritesController < ApplicationController
   def edit
   end
 
+  # GET /favourites/search
+  def search
+    @query = params[:query]
+    return unless @query.present?
+
+    require 'net/http'
+    uri = URI.parse('https://api.spotify.com/v1/search?' + {q: @query, type: 'track'}.to_query)
+    json = Net::HTTP.get(uri)
+
+    # This lets me dump out a binary value nicely
+    #render json: json
+
+    # Capture results as a hash
+    result_set = JSON.parse(json)
+    @results = result_set["tracks"]["items"]
+  end
+
   # POST /favourites
   # POST /favourites.json
   def create
--- music/config/routes.rb
+++ music/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :favourites
+  resources :favourites do
+  	collection do
+  		get 'search'
+  	end
+  end
+
   get 'songs/search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
