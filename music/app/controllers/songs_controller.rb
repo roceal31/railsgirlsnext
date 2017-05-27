a7787fb563f5786249b39e90ae7bb1f3469859f3
Andrea James <roceal31@gmail.com>
2017-05-27 10:16:27 +1000

    Submit request to Spotify API

--- music/app/controllers/songs_controller.rb
+++ music/app/controllers/songs_controller.rb
@@ -1,4 +1,13 @@
 class SongsController < ApplicationController
   def search
+  	query = params[:query]
+  	return unless query.present?
+
+  	require 'net/http'
+  	uri = URI.parse('https://api.spotify.com/v1/search?' + {q: query, type: 'track'}.to_query)
+  	json = Net::HTTP.get(uri)
+
+  	# This lets me dump out a binary value
+  	#render json: json
   end
 end
