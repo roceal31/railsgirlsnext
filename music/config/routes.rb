8f88c1bb1c883e7cf28ce46fd9497c6e2ce3f708
Andrea James <roceal31@gmail.com>
2017-05-27 16:10:21 +1000

    A bit more tidy up

--- music/config/routes.rb
+++ music/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   	end
   end
 
-  get 'songs/search'
+  get '/', to: 'favourites#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
