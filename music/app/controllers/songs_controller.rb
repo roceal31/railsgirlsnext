8b501843a746c206001a67cc2297f3c6bf5012f0
Andrea James <roceal31@gmail.com>
2017-05-27 10:42:10 +1000

    Prettying up

--- music/app/controllers/songs_controller.rb
+++ music/app/controllers/songs_controller.rb
@@ -1,10 +1,10 @@
 class SongsController < ApplicationController
   def search
-  	query = params[:query]
-  	return unless query.present?
+  	@query = params[:query]
+  	return unless @query.present?
 
   	require 'net/http'
-  	uri = URI.parse('https://api.spotify.com/v1/search?' + {q: query, type: 'track'}.to_query)
+  	uri = URI.parse('https://api.spotify.com/v1/search?' + {q: @query, type: 'track'}.to_query)
   	json = Net::HTTP.get(uri)
 
   	# This lets me dump out a binary value
